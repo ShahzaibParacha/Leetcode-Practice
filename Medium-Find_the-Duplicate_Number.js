48573cff55d4c24415614945400fffc529f89cad
Shahzaib <shahzaibperacha@gmail.com>
2020-11-12 00:12:05 -0600

    Initial commit

---
+++ Medium-Find_the-Duplicate_Number.js
@@ -0,0 +1,45 @@
+'use strict';
+var findDuplicate = function(nums) {
+    var slow = nums[0]
+    var fast = nums[0]
+
+    while(true) {
+        fast = nums[nums[fast]];
+        slow = nums[slow];
+        if (nums[fast] === nums[slow]) {
+            break;
+        }
+    };
+
+    slow = nums[0];
+    while (fast != slow) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+        
+    return slow;
+};
+
+var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,5];
+console.log(findDuplicate(nums));
+
+/* PYTHON 3 SOLUTION:
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+                
+        slow = nums[0]
+        fast = nums[0]
+        
+        while(True):
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+            
+        return slow;
+*/
---
+++ Medium-Linked_List_Cycle_II.gyp
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    def detectCycle(self, head):
+        
+        slow = head
+        fast = head.next
+        loop = None
+        
+        while slow is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                loop = fast
+        if loop is None:
+            return None
+        
+        slow = head
+        while slow != loop:
+            slow = slow.next
+            if loop.next is None:
+                return None
+            else:
+                loop = loop.next
+
+        return slow.val
+    
+headnode = ListNode(-4)
+headnode.next = (ListNode(0))
+headnode.next.next = (ListNode(2))
+headnode.next.next.next = (ListNode(3))
+
+s = Solution()
+print(s.detectCycle(headnode))
